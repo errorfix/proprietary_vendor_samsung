30fa5583e8b2e3b80871c8d5561aa9aae82ee46f
Ethan Chen <intervigil@gmail.com>
2013-10-24 17:28:19 -0700

    i9500: remove Samsung wpa_supplicant.conf
    * There's no need to use it when we have a standard one.

--- i9500/i9500-vendor-blobs.mk
+++ i9500/i9500-vendor-blobs.mk
@@ -121,5 +121,4 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/i9500/proprietary/etc/wifi/nvram_net.txt_muratafem1:system/etc/wifi/nvram_net.txt_muratafem1 \
     vendor/samsung/i9500/proprietary/etc/wifi/nvram_net.txt_muratafem2:system/etc/wifi/nvram_net.txt_muratafem2 \
     vendor/samsung/i9500/proprietary/etc/wifi/nvram_net.txt_semco3rd:system/etc/wifi/nvram_net.txt_semco3rd \
-    vendor/samsung/i9500/proprietary/etc/wifi/nvram_net.txt_semcosh:system/etc/wifi/nvram_net.txt_semcosh \
-    vendor/samsung/i9500/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    vendor/samsung/i9500/proprietary/etc/wifi/nvram_net.txt_semcosh:system/etc/wifi/nvram_net.txt_semcosh
